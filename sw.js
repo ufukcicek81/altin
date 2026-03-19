fa943ef6d1ee34fe520273e74d33d2213abb480e
ufukcicek81 <112567082+ufukcicek81@users.noreply.github.com>
2026-03-19 17:15:38 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "asil-v10";
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+});
